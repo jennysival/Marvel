3b7911c5829d004bae6c8286a83cf4be9df0fa81
jennysival <jeniffersival@gmail.com>
2022-07-04 15:58:43 -0300

    Corrigir navigation e adicionar botão

--- app/src/main/java/br/com/zup/marvel/ui/addPersonagem/view/AddPersonagemFragment.kt
+++ app/src/main/java/br/com/zup/marvel/ui/addPersonagem/view/AddPersonagemFragment.kt
@@ -6,15 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.zup.marvel.R
+import br.com.zup.marvel.databinding.FragmentAddPersonagemBinding
 
 class AddPersonagemFragment : Fragment() {
+    private lateinit var binding: FragmentAddPersonagemBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_personagem, container, false)
+    ): View {
+        binding = FragmentAddPersonagemBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
