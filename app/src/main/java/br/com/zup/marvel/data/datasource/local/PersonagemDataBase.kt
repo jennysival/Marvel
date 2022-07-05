de8dad41d648321b2cecadf71a0446b097d4c2b5
jennysival <jeniffersival@gmail.com>
2022-07-04 22:04:38 -0300

    Corigir erro de importaçãoo datatabase

--- app/src/main/java/br/com/zup/marvel/data/datasource/local/PersonagemDataBase.kt
+++ app/src/main/java/br/com/zup/marvel/data/datasource/local/PersonagemDataBase.kt
@@ -7,7 +7,6 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import br.com.zup.marvel.data.datasource.local.dao.PersonagemDAO
 import br.com.zup.marvel.domain.model.PersonagemModel
-import kotlinx.coroutines.internal.synchronized
 
 @Database(entities = [PersonagemModel::class], version = 1)
 @TypeConverters(Converters::class)
--- app/src/main/java/br/com/zup/marvel/ui/home/view/HomeActivity.kt
+++ app/src/main/java/br/com/zup/marvel/ui/home/view/HomeActivity.kt
@@ -2,17 +2,26 @@ package br.com.zup.marvel.ui.home.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import br.com.zup.marvel.R
 import br.com.zup.marvel.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
