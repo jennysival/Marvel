cf16371d227d045318aa558b78142ef77dd9c562
jennysival <jeniffersival@gmail.com>
2022-07-04 21:49:50 -0300

    Implementar o repository

--- app/src/main/java/br/com/zup/marvel/domain/repository/PersonagemRepository.kt
+++ app/src/main/java/br/com/zup/marvel/domain/repository/PersonagemRepository.kt
@@ -1,9 +1,16 @@
 package br.com.zup.marvel.domain.repository
 
+import br.com.zup.marvel.data.datasource.local.dao.PersonagemDAO
 import br.com.zup.marvel.domain.model.PersonagemModel
 
-class PersonagemRepository {
-    fun adicionarPersonagem(personagemModel: PersonagemModel){
+class PersonagemRepository(private val personagemDAO: PersonagemDAO) {
 
+    //TODO: O que é esse suspend?
+    //TODO: Esse repository está sendo usado corretamente? Inserindo e consultando os dados do DAO
+    suspend fun getAllPersonagens(): List<PersonagemModel> = personagemDAO.getAllPersonagens()
+
+    suspend fun insertPersonagem(personagem: PersonagemModel){
+        personagemDAO.insertPersonagem(personagem)
     }
+
 }
