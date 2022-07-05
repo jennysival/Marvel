c7f611d7e7f7de7ab1b81686471b9086c37badca
jennysival <jeniffersival@gmail.com>
2022-07-04 22:21:50 -0300

    Atualizar métodos da view

--- app/src/main/java/br/com/zup/marvel/ui/addpersonagem/view/AddPersonagemFragment.kt
+++ app/src/main/java/br/com/zup/marvel/ui/addpersonagem/view/AddPersonagemFragment.kt
@@ -5,13 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import br.com.zup.marvel.R
 import br.com.zup.marvel.databinding.FragmentAddPersonagemBinding
+import br.com.zup.marvel.domain.model.PersonagemModel
 import br.com.zup.marvel.ui.addpersonagem.viewmodel.AddPersonagemViewModel
 
 class AddPersonagemFragment : Fragment() {
+
+    //TODO: Como eu coloco botão de voltar nos fragmentos?
+
     private lateinit var binding: FragmentAddPersonagemBinding
+
+    //TODO: Por que usar o by lazy aqui no viewModel ao inves de inicializar direto?
+    private val viewModel: AddPersonagemViewModel by lazy {
+        ViewModelProvider(this)[AddPersonagemViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,12 +35,24 @@ class AddPersonagemFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnCadastrar.setOnClickListener {
-
+            viewModel.insertPersonagem(
+                PersonagemModel(
+                    nome = binding.etNomePersonagem.text.toString(),
+                    descricao = binding.etDescricaoPersonagem.text.toString()
+                )
+            )
         }
+
+//        initObserver()
 
         binding.btnVerLista.setOnClickListener {
             NavHostFragment.findNavController(this).navigate(R.id.action_addPersonagemFragment_to_listaPersonagensFragment)
         }
+    }
+
+    //TODO: Preciso de ajuda pra implementar o observer
+    private fun initObserver() {
+
     }
 
 }
