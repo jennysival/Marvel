ea8c9e435bf13c8575a17d3c4f5bf0860e5706bb
jennysival <jeniffersival@gmail.com>
2022-07-05 10:23:51 -0300

    Limpar imports inutilizados

--- app/src/main/java/br/com/zup/marvel/data/datasource/local/PersonagemDataBase.kt
+++ app/src/main/java/br/com/zup/marvel/data/datasource/local/PersonagemDataBase.kt
@@ -4,12 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import br.com.zup.marvel.data.datasource.local.dao.PersonagemDAO
 import br.com.zup.marvel.domain.model.PersonagemModel
 
 @Database(entities = [PersonagemModel::class], version = 1)
-@TypeConverters(Converters::class)
 abstract class PersonagemDataBase: RoomDatabase() {
     abstract fun personagemDao(): PersonagemDAO
 
